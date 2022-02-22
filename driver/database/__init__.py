e8e082a9d8006c4ef73f722a7c58c67c46448d43
Santhupodili <99010597+Santhupodili@users.noreply.github.com>
2022-02-22 06:17:33 +0530

    Create __init__.py

---
+++ driver/database/__init__.py
@@ -0,0 +1,4 @@
+""" database copyright """
+
+# thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
+# implemented by Santhu nibba (https://github.com/Santhumusicbot/santhumusic)
